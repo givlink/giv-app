441d882a9d756face97a2140d69fcfe3572e7a05
藍口康希 <kurafuto16@gmail.com>
2019-12-13 20:33:23 +0900

    made thanks

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -9,64 +9,27 @@
       </div>
     </div>
     <div class="GivList__box" id="receiveList">
-      <div class="GivList__box__term">
-        <div class="GivList__box__term__date">
-          2019.6.21
+      <!--      <div class="GivList__box__term">-->
+      <!--        <div class="GivList__box__term__date">-->
+      <!--          2019.6.21-->
+      <!--        </div>-->
+      <template v-for="item of receives">
+        <div class="GivList__box__term__content">
+          <div class="GivList__box__term__content__person">
+            <div class="GivList__box__term__content__person__icon">
+              <b-img :src="`https://api-dev.giv.link${item.giv_user.profile_image_path}`" class="GivList__box__term__content__person__icon__img" alt></b-img>
+            </div>
+            <div class="GivList__box__term__content__person__text">
+              <p class="GivList__box__term__content__person__text__name">{{ item.giv_user.last_name}} {{ item.giv_user.first_name}}</p>
+            </div>
+          </div>
+          <nuxt-link v-if="item.thanks_card == null" :to="`/thanks?id=${item.id}`" class="GivList__box__term__content__thanks">
+                    <span class="GivList__box__term__content__thanks__text">
+                        Thanksカードを<br>書きましょう
+                    </span>
+          </nuxt-link>
         </div>
-        <nuxt-link to="/thanks" class="GivList__box__term__content">
-          <div class="GivList__box__term__content__person">
-            <div class="GivList__box__term__content__person__icon">
-              <img class="GivList__box__term__content__person__icon__img" src="~/assets/image/sample_profile_icon.png" alt="giv">
-            </div>
-            <div class="GivList__box__term__content__person__text">
-              <p class="GivList__box__term__content__person__text__name">庄司 竜太郎</p>
-              <p class="GivList__box__term__content__person__text__position">CO-FOUNDER & CCO</p>
-            </div>
-          </div>
-          <div class="GivList__box__term__content__thanks">
-                    <span class="GivList__box__term__content__thanks__text">
-                        Thanksカードを<br>書きましょう
-                    </span>
-          </div>
-        </nuxt-link>
-        <nuxt-link to="/thanks" class="GivList__box__term__content">
-          <div class="GivList__box__term__content__person">
-            <div class="GivList__box__term__content__person__icon">
-              <img class="GivList__box__term__content__person__icon__img" src="~/assets/image/sample_profile_icon.png" alt="giv">
-            </div>
-            <div class="GivList__box__term__content__person__text">
-              <p class="GivList__box__term__content__person__text__name">庄司 竜太郎</p>
-              <p class="GivList__box__term__content__person__text__position">CO-FOUNDER & CCO</p>
-            </div>
-          </div>
-          <div class="GivList__box__term__content__thanks">
-                    <span class="GivList__box__term__content__thanks__text">
-                        Thanksカードを<br>書きましょう
-                    </span>
-          </div>
-        </nuxt-link>
-      </div>
-      <div class="GivList__box__term">
-        <div class="GivList__box__term__date">
-          2019.6.22
-        </div>
-        <nuxt-link to="/thanks" class="GivList__box__term__content">
-          <div class="GivList__box__term__content__person">
-            <div class="GivList__box__term__content__person__icon">
-              <img class="GivList__box__term__content__person__icon__img" src="~/assets/image/sample_profile_icon.png" alt="giv">
-            </div>
-            <div class="GivList__box__term__content__person__text">
-              <p class="GivList__box__term__content__person__text__name">庄司 竜太郎</p>
-              <p class="GivList__box__term__content__person__text__position">CO-FOUNDER & CCO</p>
-            </div>
-          </div>
-          <div class="GivList__box__term__content__thanks">
-                    <span class="GivList__box__term__content__thanks__text">
-                        Thanksカードを<br>書きましょう
-                    </span>
-          </div>
-        </nuxt-link>
-      </div>
+      </template>
     </div>
   </div>
 </template>
@@ -74,6 +37,8 @@
 <script>
 
     const Cookie = process.client ? require('js-cookie') : undefined;
+    import axios from 'axios';
+    import moment from 'moment';
     export default {
         components: {
         },
@@ -83,6 +48,29 @@
             return {
                 code: '',
                 hasError: '',
+            }
+        },
+        async asyncData({ app }) {
+            const baseUrl = process.env.baseUrl + '/me/receive';
+            const getUrl = encodeURI(baseUrl);
+            const token = app.$auth.$storage.getUniversal("_token.auth0");
+            const response = await axios.get(getUrl, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: token
+                }
+            });
+            const baseUrl2 = process.env.baseUrl + '/me/giv';
+            const getUrl2 = encodeURI(baseUrl2);
+            const response2 = await axios.get(getUrl2, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: token
+                }
+            });
+            return {
+                receives: response.data.givs,
+                givs: response2.data.givs,
             }
         },
         mounted() {
