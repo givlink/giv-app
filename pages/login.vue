ec8eab4da2f7c337e16a8381c133a5511508d126
Kashif <static.kash@gmail.com>
2021-02-04 14:55:53 +0900

    fix auth on mobile

--- pages/login.vue
+++ pages/login.vue
@@ -37,35 +37,33 @@ export default {
   },
   mounted() {
     this.loading = this.$store.state.authLoading;
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then(result => {
+        console.log("Got user: ", result);
+      })
+      .catch(err => {
+        console.log("Got err:", err);
+        this.hasError = err.message;
+      });
   },
   methods: {
     async login() {
       const provider = new firebase.auth.FacebookAuthProvider();
-      try {
-        const user = await firebase.auth().signInWithPopup(provider);
-        const u = {
-          name: user.displayName,
-          id: user.uid
-        };
-        this.$store.commit("setUser", u);
-        this.$router.push({ path: "/" });
-      } catch (err) {
-        if (err.code === "auth/popup-blocked") {
-          try {
-            const user = await firebase.auth().signInWithRedirect(provider);
-            const u = {
-              name: user.displayName,
-              id: user.uid
-            };
-            this.$store.commit("setUser", u);
-            this.$router.push({ path: "/" });
-          } catch (err) {
-            this.hasError = err.message;
-          }
-        } else {
-          this.hasError = error.message;
-        }
-      }
+      firebase.auth().signInWithRedirect(provider);
+      //Below has issues on mobile
+      /* try { */
+      /*   const user = await firebase.auth().signInWithPopup(provider); */
+      /*   const u = { */
+      /*     name: user.displayName, */
+      /*     id: user.uid */
+      /*   }; */
+      /*   this.$store.commit("setUser", u); */
+      /*   this.$router.push({ path: "/" }); */
+      /* } catch (err) { */
+      /*     this.hasError = error.message; */
+      /* } */
     }
   }
 };
