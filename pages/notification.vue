1297ff005bd90a11eaa55e6f1b11154ece665cd2
Kashif <static.kash@gmail.com>
2021-02-05 18:23:48 +0900

    fix giv page, fix notifications page

--- pages/notification.vue
+++ pages/notification.vue
@@ -1,97 +1,85 @@
 <template>
   <div class="Notification Main">
     <ul class="Notification__list">
-      <template v-for="item of notifications">
-        <template v-if="item.event_type == 'send_giv'">
-          <li class="Notification__list__li">
-            <nuxt-link
-              :to="`/users/${item.from_user.id}`"
-              class="Notification__list__li__link"
-            >
-              <div class="Notification__list__li__link__icon">
-                <b-img
-                  :src="`${basePath}${item.from_user.profile_image_path}`"
-                  class="Notification__list__li__link__icon__img"
-                  alt
-                ></b-img>
-              </div>
-              <div class="Notification__list__li__link__text">
-                <p class="Notification__list__li__link__text__info">
-                  「{{ item.from_user.last_name }}
-                  {{
-                    item.from_user.first_name
-                  }}」さんからgivを贈りたいのアクションがありました
-                </p>
-                <p class="Notification__list__li__link__text__date">
-                  {{ item.created_at | moment }}
-                </p>
-              </div>
-            </nuxt-link>
-          </li>
-        </template>
-        <template v-else-if="item.event_type == 'want_giv'">
-          <li class="Notification__list__li">
-            <nuxt-link
-              :to="`/users/${item.from_user.id}`"
-              class="Notification__list__li__link"
-            >
-              <div class="Notification__list__li__link__icon">
-                <b-img
-                  :src="`${basePath}${item.from_user.profile_image_path}`"
-                  class="Notification__list__li__link__icon__img"
-                  alt
-                ></b-img>
-              </div>
-              <div class="Notification__list__li__link__text">
-                <p class="Notification__list__li__link__text__info">
-                  「{{ item.from_user.last_name }}
-                  {{
-                    item.from_user.first_name
-                  }}」さんからgivを受け取りたいのアクションがありました
-                </p>
-                <p class="Notification__list__li__link__text__date">
-                  {{ item.created_at | moment }}
-                </p>
-              </div>
-            </nuxt-link>
-          </li>
-        </template>
-        <template v-else-if="item.event_type == 'thanks_card'">
-          <li class="Notification__list__li">
-            <nuxt-link
-              :to="`/thanks?id=${getThanksJsonData(item.detail_json)}`"
-              class="Notification__list__li__link"
-            >
-              <div class="Notification__list__li__link__text">
-                <p class="Notification__list__li__link__text__info">
-                  サンクスカードを書きましょう
-                </p>
-                <p class="Notification__list__li__link__text__date">
-                  {{ item.created_at | moment }}
-                </p>
-              </div>
-            </nuxt-link>
-          </li>
-        </template>
-
-        <template v-else-if="item.event_type == 'giv'">
-          <li class="Notification__list__li">
-            <nuxt-link
-              :to="`${getJsonData(item.detail_json)}`"
-              class="Notification__list__li__link"
-            >
-              <div class="Notification__list__li__link__text">
-                <p class="Notification__list__li__link__text__info">
-                  新しいgivがあります
-                </p>
-                <p class="Notification__list__li__link__text__date">
-                  {{ item.created_at | moment }}
-                </p>
-              </div>
-            </nuxt-link>
-          </li>
-        </template>
-      </template>
+      <li class="Notification__list__li" v-for="item of pendingPostGivs">
+        <nuxt-link
+          :to="`/posts/create?givId=${item.id}`"
+          class="Notification__list__li__link"
+        >
+          <div class="Notification__list__li__link__text">
+            <p class="Notification__list__li__link__text__info">
+              サンクスカードを書きましょう
+            </p>
+            <p class="Notification__list__li__link__text__date">
+              {{ item.createdAt | moment }}
+            </p>
+          </div>
+        </nuxt-link>
+      </li>
+    </ul>
+    <ul class="Notification__list" v-for="item of newGivs">
+      <li class="Notification__list__li">
+        <nuxt-link :to="`/giv/${item.id}`" class="Notification__list__li__link">
+          <div class="Notification__list__li__link__text">
+            <p class="Notification__list__li__link__text__info">
+              新しいgivがあります
+            </p>
+            <p class="Notification__list__li__link__text__date">
+              {{ item.createdAt | moment }}
+            </p>
+          </div>
+        </nuxt-link>
+      </li>
+    </ul>
+    <ul class="Notification__list">
+      <li class="Notification__list__li" v-for="item of givRequests">
+        <nuxt-link
+          :to="`/users/${item.senderId}`"
+          class="Notification__list__li__link"
+          v-if="item.type == 'send'"
+        >
+          <div class="Notification__list__li__link__icon">
+            <b-img
+              :src="`${item.sender.photoURL}`"
+              class="Notification__list__li__link__icon__img"
+              alt
+            ></b-img>
+          </div>
+          <div class="Notification__list__li__link__text">
+            <p class="Notification__list__li__link__text__info">
+              「{{
+                item.sender.name
+              }}」さんからgivを贈りたいのアクションがありました
+            </p>
+            <p class="Notification__list__li__link__text__date">
+              {{ item.createdAt | moment }}
+            </p>
+          </div>
+        </nuxt-link>
+        <nuxt-link
+          :to="`/users/${item.senderId}`"
+          class="Notification__list__li__link"
+          v-if="item.type == 'receive'"
+        >
+          <div class="Notification__list__li__link__icon">
+            <b-img
+              :src="`${item.receiver.photoURL}`"
+              class="Notification__list__li__link__icon__img"
+              alt
+            ></b-img>
+          </div>
+          <div class="Notification__list__li__link__text">
+            <p class="Notification__list__li__link__text__info">
+              「{{
+                item.receiver.name
+              }}」さんからgivを贈りたいのアクションがありました
+            </p>
+            <p class="Notification__list__li__link__text__date">
+              {{ item.createdAt | moment }}
+            </p>
+          </div>
+        </nuxt-link>
+      </li>
     </ul>
   </div>
 </template>
@@ -102,38 +90,38 @@ import api from "../lib/api";
 
 export default {
   layout: "logined",
+  data() {
+    return {
+      pendingPostGivs: [],
+      postsForMe: [],
+      givs: []
+    };
+  },
   async asyncData({ app }) {
     const { uid } = api.getCurrentUser();
+    const finishedGivs = await api.getFinishedGivs(uid);
+    const newGivs = await api.getNewGivs(uid);
+    const givRequests = await api.getGivRequests(uid);
+    const postsForMe = await api.getPostsForMe(uid);
+    const pendingPostGivs = [];
+    finishedGivs.forEach(async giv => {
+      const post = await api.getPostByGivId(giv.id);
+      if (!post) {
+        pendingPostGivs.push(giv);
+      }
+    });
     return {
-      notifications: await api.listNotifications(uid)
+      notifications: await api.listNotifications(uid),
+      pendingPostGivs,
+      postsForMe,
+      newGivs,
+      givRequests
     };
   },
 
   filters: {
     moment: function(date) {
       return moment(date).format("YYYY.MM.DD");
-    }
-  },
-  methods: {
-    getJsonData: function(json) {
-      if (json && json !== "") {
-        const obj = JSON.parse(json);
-        if (obj.receive_id) {
-          return "/receive/" + obj.receive_id;
-        } else if (obj.giv_id) {
-          return "/giv/" + obj.giv_id;
-        }
-      }
-      return "";
-    },
-    getThanksJsonData: function(json) {
-      if (json && json !== "") {
-        const obj = JSON.parse(json);
-        if (obj.receive_id) {
-          return obj.receive_id;
-        }
-      }
-      return "";
     }
   }
 };
