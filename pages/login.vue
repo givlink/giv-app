ce13ef153011b552dd85c6ef00992de40a1b16ae
Kashif <static.kash@gmail.com>
2021-04-01 17:08:14 +0900

    fix pushtoken

--- pages/login.vue
+++ pages/login.vue
@@ -7,11 +7,7 @@
     </div>
     <div class="Inner Invite__inner" v-show="!loading">
       <div class="Invite__logo mx-auto">
-        <img
-          class="Invite__logo__img"
-          src="~/assets/image/giv_logo.png"
-          alt="giv"
-        />
+        <img class="Invite__logo__img" src="~/assets/image/giv_logo.png" alt="giv" />
       </div>
       <div class="Invite__form">
         <div class="Invite__btn">
@@ -30,18 +26,12 @@
         </div>
         <p class="Invite__error">{{ hasError }}</p>
       </div>
-      <div
-        class="fixed flex items-center justify-center bottom-0 left-0 right-0 mb-12"
-      >
+      <div class="fixed flex items-center justify-center bottom-0 left-0 right-0 mb-12">
         <button
           v-on:click="login('apple')"
           class="flex items-center leading-0 text-sm shadow-sm hover:shadow-xl transition duration-150 rounded-md px-4 py-2 bg-gray-900 font-semibold text-white"
         >
-          <img
-            class="h-5 w-5 mr-2"
-            src="~/assets/image/apple_logo.svg"
-            alt="apple"
-          />
+          <img class="h-5 w-5 mr-2" src="~/assets/image/apple_logo.svg" alt="apple" />
           <span class="mt-1">Sign in with Apple</span>
         </button>
       </div>
@@ -56,18 +46,22 @@ export default {
   data() {
     return {
       hasError: "",
-      loading: true
+      loading: true,
     };
   },
   mounted() {
     this.loading = this.$store.state.authLoading;
+    if (this.$route.query.pushtoken) {
+      this.$store.commit("setPushToken", this.$route.query.pushtoken);
+      localStorage.setItem("pushToken", this.$route.query.pushtoken);
+    }
     firebase
       .auth()
       .getRedirectResult()
-      .then(result => {
+      .then((result) => {
         console.log("Got user: ", result);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log("Got err:", err);
         this.hasError = err.message;
       });
@@ -79,8 +73,8 @@ export default {
         provider = new firebase.auth.OAuthProvider("apple.com");
       }
       firebase.auth().signInWithRedirect(provider);
-    }
-  }
+    },
+  },
 };
 </script>
 
