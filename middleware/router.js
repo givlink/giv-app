8aee03a3b3f9fc1849eeacbfe38d0e00017d5121
Kashif <static.kash@gmail.com>
2021-04-02 15:41:43 +0900

    set pustoken in login page

--- middleware/router.js
+++ middleware/router.js
@@ -1,5 +1,8 @@
 export default function({ store, redirect, route }) {
   if (store.state.user && route.name === "login") {
+    if (route.query.pushtoken) {
+      localStorage.setItem("pushToken", route.query.pushtoken);
+    }
     redirect("/");
   }
 
