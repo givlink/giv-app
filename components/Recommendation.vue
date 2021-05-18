96226774f3e899311fc3beb88f045db1dcec1794
Kashif <static.kash@gmail.com>
2021-05-18 14:46:33 +0900

    update heading style

--- components/Recommendation.vue
+++ components/Recommendation.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="User__giv" v-if="!recommendationsLoading && users.length > 0">
     <h3 class="User__giv__title">{{ getLabel }}</h3>
-    <ul class="flex items-center overflow-x-auto overflow-y-hidden space-x-3">
+    <span style="color:#42A8E4;" class="font-bold">{{ getLabel2 }}</span>
+    <ul
+      class="mt-0.5 flex items-center overflow-x-auto overflow-y-hidden space-x-3"
+    >
       <UserCircleItem
         :user="user"
         class=""
@@ -18,20 +21,32 @@ import UserCircleItem from "@/components/UserCircleItem";
 import { mapState } from "vuex";
 
 export default {
-  props: ["type", "label"],
+  props: ["type", "label", "label2"],
   computed: {
     ...mapState(["recommendations", "recommendationsLoading"]),
     users() {
       return this.$utils.shuffleArray(this.recommendations[this.type]);
+    },
+    getLabel2() {
+      if (this.label2) return this.label2;
+
+      switch (this.type) {
+        case "matchingYourInterests":
+          return "なたが興味/関心あるギブを提供できるメンバー";
+        case "matchingYourSkills":
+          return "あなたのギブに興味/関心があるメンバー";
+      }
+
+      return null;
     },
     getLabel() {
       if (this.label) return this.label;
 
       switch (this.type) {
         case "matchingYourInterests":
-          return "受け取ってみませんか？　あなたが興味/関心あるギブを提供できるメンバー";
+          return "受け取ってみませんか？";
         case "matchingYourSkills":
-          return "贈ってみませんか？　あなたのギブに興味/関心があるメンバー";
+          return "贈ってみませんか？";
         case "similarInterests":
           return "あなたと同じ興味/関心を持つメンバー";
       }
